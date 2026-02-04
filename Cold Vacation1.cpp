850b4663ebeb0cbf5bacc28d96f6a7ae58ebf974
MonaFan1 <2710361560@qq.com>
2026-02-04 19:13:52 +0800

    Update Cold Vacation1.cpp

--- Cold Vacation1.cpp
+++ Cold Vacation1.cpp
@@ -1,47 +1,37 @@
 #include <iostream>
-
 template <typename T>
 class LimitValue {
-private:
+public:
     T current_value;
     T min_value;
     T max_value;
 
-    void clamp() {
+    void fun() {
         if (current_value < min_value) current_value = min_value;
         else if (current_value > max_value) current_value = max_value;
     }
-
-public:
-    LimitValue(T init_val, T min_val, T max_val)
-        : current_value(init_val), min_value(min_val), max_value(max_val) {
-        clamp();
+    
+    LimitValue(T t, T m_min, T m_max) : current_value(t), min_value(m_min), max_value(m_max) {
+        fun();
     }
-
-    LimitValue<T>& operator=(const T& new_val) {
-        current_value = new_val;
-        clamp();
-        return *this;
+    void operator=(T val) {
+        current_value = val;
+        fun();
     }
-
-    LimitValue<T>& operator+=(const T& add_val) {
-        current_value += add_val;
-        clamp();
-        return *this;
+    void operator+=(T val) {
+        current_value += val;
+        fun();
     }
-
-    T val() const {
-        return current_value;
-    }
+    T val() { return current_value; }
 };
 
 int main() {
     LimitValue<int> pwm(50, 0, 100);
-    pwm += 60;
-    std::cout << "PWM值：" << pwm.val() << std::endl;
+    pwm += 1000000;
+    std::cout << "PWM=" << pwm.val() << std::endl;
 
-    pwm = -20;
-    std::cout << "PWM值：" << pwm.val() << std::endl;
+    pwm = -1000000;
+    std::cout << "PWM=" << pwm.val() << std::endl;
 
     return 0;
 }
